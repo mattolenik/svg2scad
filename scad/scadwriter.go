8965a78e56e4c2c60be3e757b1f09fa4b725adf9
Matt Olenik <olenikm@gmail.com>
2024-09-15 14:19:09 -0700

    Link things together with variable references in codegen

--- scad/scadwriter.go
+++ scad/scadwriter.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path/filepath"
 	"reflect"
+	"strings"
 
 	"github.com/k0kubun/pp/v3"
 	"github.com/mattolenik/svg2scad/std"
@@ -56,8 +57,13 @@ func (sw *SCADWriter) ConvertSVGToSCAD(svg *svg.SVG, output io.Writer) error {
 		if err != nil {
 			return fmt.Errorf("failed to parse path from SVG %q: %w", path, err)
 		}
-		err = sw.walk(cw, tree, found, &ast.Coord{0, 0}, path.ID)
-		if err != nil {
+		if err := sw.walk(cw, tree,
+			&walkState{
+				foundModule: found,
+				lastPoint:   ast.Coord{0, 0}.String(),
+				pathID:      path.ID,
+			},
+		); err != nil {
 			return fmt.Errorf("failed to generate OpenSCAD code: %w", err)
 		}
 		pp.Println(tree)
@@ -68,45 +74,58 @@ func (sw *SCADWriter) ConvertSVGToSCAD(svg *svg.SVG, output io.Writer) error {
 	return cw.Write(output)
 }
 
-func (sw *SCADWriter) walk(cw *ast.CodeWriter, node any, foundModule func(m *ast.Module), lastPoint *ast.Coord, pathID string) (err error) {
+type walkState struct {
+	lastPoint   string
+	pathID      string
+	foundModule func(*ast.Module)
+}
+
+func (sw *SCADWriter) walk(cw *ast.CodeWriter, node any, state *walkState) (err error) {
 	switch node := node.(type) {
 	case []any:
 		for _, n := range node {
-			err := sw.walk(cw, n, foundModule, lastPoint, pathID)
+			err := sw.walk(cw, n, state)
 			if err != nil {
 				return err
 			}
 		}
+
 	case *ast.MoveTo:
-		*lastPoint = node.Coord
+		state.lastPoint = "cursor"
 		cw.Lines(fmt.Sprintf("let(cursor = %v)", node.Coord))
 		if err := cw.BraceIndent(func() error {
-			return sw.walk(cw, node.Children, foundModule, lastPoint, pathID)
+			return sw.walk(cw, node.Children, state)
 		}); err != nil {
 			return err
 		}
+
 	case *ast.Bezier:
-		coords := ast.Coords(append([]ast.Coord{*lastPoint}, node.Points...))
-		cw.Linef("%s = bezier_curve(%v, splinesteps = %d);", node.Name, coords, sw.SplineSteps)
-		cw.Linef(`stroke(%s, width = %d);`, node.Name, sw.StrokeWidth)
-		*lastPoint = node.Points.End()
+		coords := append([]string{state.lastPoint}, std.Map(node.Points, func(v *ast.Coord) string { return v.String() })...)
+		cw.Linef("%s = [ %v ];", node.Name, strings.Join(coords, ", "))
+		cw.Linef(`stroke(bezier_curve(%s, splinesteps = %d), width = %d);`, node.Name, sw.SplineSteps, sw.StrokeWidth)
+		// state.lastPoint = node.Points[len(node.Points)-1].String()
+		state.lastPoint = fmt.Sprintf("%s[%d]", node.Name, len(node.Points))
+
 	case *ast.LineTo:
 		// TODO: line command
+
 	case *ast.ClosePath:
 		// TODO: close path
+
 	case *ast.Module:
-		if pathID != "" {
-			node.Name = pathID
+		if state.pathID != "" {
+			node.Name = state.pathID
 		}
 		cw.Linef("module %s(anchor, spin, orient)", node.Name)
 
 		if err := cw.BraceIndent(func() error {
-			return sw.walk(cw, node.Children, foundModule, lastPoint, pathID)
+			return sw.walk(cw, node.Children, state)
 		}); err != nil {
 			return err
 		}
 
-		foundModule(node)
+		state.foundModule(node)
+
 	default:
 		return fmt.Errorf("unsupported command: %q", reflect.TypeOf(node))
 	}
--- std/std.go
+++ std/std.go
@@ -1,10 +1,88 @@
 package std
 
-import "strings"
+import (
+	"fmt"
+	"io"
+	"os"
+	"reflect"
+	"strings"
+)
+
+func Map[TIn any, TOut any](items []TIn, mapFn func(v *TIn) TOut) []TOut {
+	result := make([]TOut, len(items))
+	for i := range items {
+		result[i] = mapFn(&items[i])
+	}
+	return result
+}
 
 func EnsureSuffix(str, suffix string) string {
 	if strings.HasSuffix(str, suffix) {
 		return str
 	}
 	return str + suffix
+}
+
+// Fdump writes the structure's fields to the given writer
+func Fdump(w io.Writer, v any) {
+	printFieldsRecursive(w, reflect.ValueOf(v), 0, true)
+}
+
+// Dump writes the structure's fields to stdout
+func Dump(v any) {
+	Fdump(os.Stdout, v)
+}
+
+// Recursive function to print all public fields of a struct
+func printFieldsRecursive(w io.Writer, val reflect.Value, indentLevel int, shouldIndent bool) {
+	isPtr := val.Kind() == reflect.Ptr
+	if isPtr {
+		val = val.Elem()
+	}
+	indent := strings.Repeat("  ", indentLevel)
+	// Handle different kinds
+	switch val.Kind() {
+	case reflect.Struct:
+		fmt.Fprintf(w, "%s\n", niceType(val.Type(), isPtr))
+		typ := val.Type()
+		for i := 0; i < val.NumField(); i++ {
+			fieldVal := val.Field(i)
+			fieldType := typ.Field(i)
+
+			// Only print public fields (those starting with uppercase letters)
+			if fieldType.PkgPath == "" {
+				fmt.Fprintf(w, "%s%s: ", indent, fieldType.Name)
+				printFieldsRecursive(w, fieldVal, indentLevel+1, false)
+			}
+		}
+	case reflect.Slice, reflect.Array:
+		fmt.Fprintf(w, "[\n")
+		for i := 0; i < val.Len(); i++ {
+			printFieldsRecursive(w, val.Index(i), indentLevel+1, true)
+		}
+		fmt.Fprintf(w, "%s]\n", indent)
+	case reflect.Map:
+		fmt.Fprintf(w, "%s{\n", niceType(val.Type(), isPtr))
+		for _, key := range val.MapKeys() {
+			indent := strings.Repeat("  ", indentLevel+1)
+			fmt.Fprintf(w, "%s%s: ", indent, key)
+			printFieldsRecursive(w, val.MapIndex(key), indentLevel+1, shouldIndent)
+		}
+		fmt.Fprintf(w, "%s}\n", indent)
+	default:
+		// Base case: print the value of leaf nodes
+		indent := indent
+		if !shouldIndent {
+			indent = ""
+		}
+		fmt.Fprintf(w, "%s%s %v\n", indent, niceType(val.Type(), isPtr), val.Interface())
+	}
+}
+
+func niceType(typ reflect.Type, isPtr bool) string {
+	p := ""
+	if isPtr {
+		p = "*"
+	}
+	return "｢" + p + strings.ReplaceAll(typ.String(), "interface {}", "any") + "｣"
 }
